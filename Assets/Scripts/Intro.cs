1389e450899d06f572ac964d84311bed35e3815d
thelastpointer <thelastpointer@gmail.com>
2016-01-30 17:09:16 +0100

    intro coming up

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour
+{
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController Controller;
+    public GameObject SpeechBubble;
+    public UnityEngine.UI.Text SpeechText;
+    public SpeechSequence[] Sequence1;
+    public SpeechSequence[] Sequence2;
+    public float DisableControlsForSeconds;
+
+    float defWalkSpeed;
+
+    [System.Serializable]
+    public class SpeechSequence
+    {
+        public string Text;
+        public float Time;
+    }
+
+	void Start()
+    {
+        defWalkSpeed = Controller.m_WalkSpeed;
+        Controller.m_WalkSpeed = 0;
+        Controller.m_RunSpeed = 0;
+
+        StartCoroutine(IntroSeq());
+        Invoke("EnableControls", DisableControlsForSeconds);
+    }
+
+    IEnumerator IntroSeq()
+    {
+        foreach (SpeechSequence seq in Sequence1)
+        {
+            SpeechBubble.SetActive(true);
+            SpeechText.text = seq.Text;
+            yield return new WaitForSeconds(seq.Time);
+        }
+    }
+
+    public void PlaySequence2()
+    {
+        StartCoroutine(Seq2());
+    }
+
+    IEnumerator Seq2()
+    {
+        foreach (SpeechSequence seq in Sequence2)
+        {
+            SpeechBubble.SetActive(true);
+            SpeechText.text = seq.Text;
+            yield return new WaitForSeconds(seq.Time);
+        }
+    }
+
+    void EnableControls()
+    {
+        Controller.m_WalkSpeed = defWalkSpeed;
+        Controller.m_RunSpeed = defWalkSpeed;
+    }
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -10,7 +10,7 @@ public class Inventory : MonoBehaviour
     public GameObject HandIcon;
 
     Pickup leftHand, rightHand;
-    Pickup lookAt = null;
+    Usable lookAt = null;
     Vector3 leftOriginalScale, rightOriginalScale;
 
     void Update()
@@ -19,89 +19,62 @@ public class Inventory : MonoBehaviour
         RaycastHit hit;
         lookAt = null;
         if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0)), out hit, 1.5f, 1 << LayerMask.NameToLayer("Item")))
-            lookAt = hit.collider.GetComponent<Pickup>();
+            lookAt = hit.collider.GetComponent<Usable>();
 
+        // UI icon
         HandIcon.SetActive(lookAt != null);
 
         // Left hand
         if (Input.GetMouseButtonDown(0))
         {
-            // Pick up
-            if ((leftHand == null) && (lookAt != null))
+            // Pick up stuff
+            if ((lookAt != null) && (lookAt.Item != null))
             {
-                Audio.PlayOneShot(PickupSound);
-                leftHand = lookAt;
-                leftOriginalScale = lookAt.transform.localScale;
-                Rigidbody rb = lookAt.GetComponent<Rigidbody>();
-                if (rb != null)
-                    rb.isKinematic = true;
-                lookAt.transform.SetParent(LeftHandContainer);
-                lookAt.transform.localPosition = Vector3.zero;
-                lookAt.transform.localRotation = Quaternion.identity;
-            }
-            // Unable to pick up
-            else if ((leftHand != null) && (lookAt != null))
-            {
-                // TODO: Sad sound
-            }
-            // Throw shit
-            else if ((leftHand != null) && (lookAt == null))
-            {
-                // TODO: Triggers and stuff
-
+                if ((leftHand == null) && (lookAt != null) && (lookAt.Item != null))
                 {
-                    leftHand.transform.SetParent(null);
-                    leftHand.transform.localScale = leftOriginalScale;
-                    Rigidbody rb = leftHand.GetComponent<Rigidbody>();
+                    Audio.PlayOneShot(PickupSound);
+                    leftHand = lookAt.Item;
+                    leftOriginalScale = lookAt.transform.localScale;
+                    Rigidbody rb = lookAt.GetComponent<Rigidbody>();
                     if (rb != null)
-                    {
-                        rb.isKinematic = false;
-                        rb.velocity = GetComponentInParent<Rigidbody>().velocity;
-                    }
-
-                    leftHand = null;
+                        rb.isKinematic = true;
+                    lookAt.transform.SetParent(LeftHandContainer);
+                    lookAt.transform.localPosition = Vector3.zero;
+                    lookAt.transform.localRotation = Quaternion.identity;
                 }
+            }
+            // Activate trigger
+            if ((lookAt != null) && (lookAt.Trigger != null))
+            {
+                if (!string.IsNullOrEmpty(lookAt.NeedsItem) && (string.Compare(leftHand.Item, lookAt.NeedsItem) == 0))
+                {
+                    lookAt.Trigger.Invoke();
+                    if (lookAt.ConsumesItem)
+                    {
+                        Destroy(leftHand.gameObject);
+                    }
+                }
+            }
+            
+            // Throw what's in out hands
+            if ((leftHand != null) && (lookAt == null))
+            {
+                leftHand.transform.SetParent(null);
+                leftHand.transform.localScale = leftOriginalScale;
+                Rigidbody rb = leftHand.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.isKinematic = false;
+                    rb.velocity = GetComponentInParent<Rigidbody>().velocity;
+                }
+
+                leftHand = null;
             }
         }
         // Right hand
         if (Input.GetMouseButtonDown(1))
         {
-            // Pick up
-            if ((rightHand == null) && (lookAt != null))
-            {
-                Audio.PlayOneShot(PickupSound);
-                rightHand = lookAt;
-                rightOriginalScale = lookAt.transform.localScale;
-                Rigidbody rb = lookAt.GetComponent<Rigidbody>();
-                if (rb != null)
-                    rb.isKinematic = true;
-                lookAt.transform.SetParent(RightHandContainer);
-                lookAt.transform.localPosition = Vector3.zero;
-                lookAt.transform.localRotation = Quaternion.identity;
-            }
-            // Unable to pick up
-            else if ((rightHand != null) && (lookAt != null))
-            {
-                // TODO: Sad sound
-            }
-            // Throw shit
-            else if ((rightHand != null) && (lookAt == null))
-            {
-                // TODO: Triggers and stuff
-
-                {
-                    rightHand.transform.SetParent(null);
-                    rightHand.transform.localScale = rightOriginalScale;
-                    Rigidbody rb = rightHand.GetComponent<Rigidbody>();
-                    if (rb != null)
-                    {
-                        rb.isKinematic = false;
-                        rb.velocity = GetComponentInParent<Rigidbody>().velocity;
-                    }
-
-                    rightHand = null;
-                }
-            }
+            
         }
         // Combine
         if (Input.GetMouseButtonDown(2))
@@ -118,10 +91,6 @@ public class Inventory : MonoBehaviour
                     Vector3 pos = Camera.main.transform.TransformPoint(new Vector3(0f, 0f, 3f));
                     GameObject go = (GameObject)Instantiate(result, pos, Quaternion.identity);
                     go.SetActive(true);
-                }
-                else
-                {
-                    //...saaaad sound
                 }
             }
         }
